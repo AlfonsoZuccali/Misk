66b552a3067141cfc14f4e03e286fb5f0506b321
Alfonso Zuccali <alfonso.zuccali@gmail.com>
2026-02-08 23:40:42 -0300

    added the initialization for inputManager

--- src/main.cpp
+++ src/main.cpp
@@ -3,22 +3,32 @@
 #include "secrets.h"
 #include "timeManager.h"
 #include "DisplayManager.h"
+#include "inputManager.h"
+#include "pomodoroManager.h"
 
 WiFiManager wifiManager(WIFI_SSID,WIFI_PASSWORD);
 TimeManager timeManager(NTP_SERVER, GMT_OFFSET_SEC, DAYLIGHT_OFFSET_SEC);
 DisplayManager displayManager;
+InputManager inputManager;
+PomodoroManager pomodoroManager;
+
 //we define the event group that wil be used by the system
 EventGroupHandle_t systemEventGroup;
+QueueHandle_t commandQueue;
 
 void setup(){
   Serial.begin(115200);
 
   //we create the event group
   systemEventGroup = xEventGroupCreate();
+  commandQueue = xQueueCreate(10, sizeof(SystemCommand));
 
   //we initiate the wifi and delegate the task creation to the object
   wifiManager.startConnectionTask(systemEventGroup);
   timeManager.startTimeTask(systemEventGroup);
+
+  inputManager.begin(commandQueue);     
+  //pomodoroManager.begin(commandQueue);
 
   displayManager.startTask();
 }
